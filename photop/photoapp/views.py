4490e1347c2c8a5b08985278ece466fce8932a00
Gearboxpetrolhead <57763741+Gearboxpetrolhead@users.noreply.github.com>
2021-02-15 19:42:13 +0300

    initial version

---
+++ photop/photoapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+from .models import Photo
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+
+def index(request):
+
+    all_photos = Photo.objects.all()
+    
+    context = {
+        'all_photos' : all_photos
+    }
+    return render(request,'photoapp/index.html', context)
+
+def detail(request, photo_id):
+    try:
+        photo = Photo.objects.get(id=photo_id)
+    except Photo.DoesNotExist:
+        raise Http404('Photo not found')
+
+    return render(request, 'photoapp/detail.html', {'photo':photo})
